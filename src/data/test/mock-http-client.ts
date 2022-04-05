369f1d8c6ec0f4cce982275371ae112804accf03
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-04-05 15:50:22 -0300

    test: ensure RemoteAuthentication throws UnexpectedError on default cases

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,14 +1,11 @@
-import {
-  HttpPostClient,
-  HttpPostParams
-} from '../protocols/http/http-post-client';
+import { HttpPostClient, HttpPostParams } from '../protocols/http/http-post-client';
 import { HttpResponse, HttpStatusCode } from '../protocols/http/http-response';
 
 export class HttpPostClientSpy implements HttpPostClient {
   url?: string;
   body?: object;
   response: HttpResponse = {
-    statusCode: HttpStatusCode.noContent
+    statusCode: HttpStatusCode.success
   };
 
   async post(params: HttpPostParams): Promise<HttpResponse> {
