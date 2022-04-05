1a5f80f7252b72ae7a68878ba1d3462d28d762bb
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-04-05 15:39:35 -0300

    feat: ensure RemoteAuthentication throws InvalidCredentialsError on 401

---
+++ src/data/protocols/http/http-response.ts
@@ -0,0 +1,12 @@
+export enum HttpStatusCode {
+  success = 200,
+  created = 201,
+  noContent = 204,
+  unauthorized = 401
+}
+
+export type HttpResponse = {
+  statusCode: HttpStatusCode;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  body?: any;
+};
