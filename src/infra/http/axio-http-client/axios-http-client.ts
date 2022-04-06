d393b4a97ca1da6696a586de8a48f4016f46f55a
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-04-06 11:33:15 -0300

    feat: ensure AxiosHttpClient returns the correct statusCode and body

--- src/infra/http/axio-http-client/axios-http-client.ts
+++ src/infra/http/axio-http-client/axios-http-client.ts
@@ -1,9 +1,14 @@
-import { HttpPostParams } from '@/data/protocols/http';
+import { HttpPostClient, HttpPostParams, HttpResponse } from '@/data/protocols/http';
 import axios from 'axios';
 
-export class AxiosHttpClient {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export class AxiosHttpClient implements HttpPostClient<any, any> {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  async post(params: HttpPostParams<any>): Promise<void> {
-    await axios.post(params.url, params.body);
+  async post(params: HttpPostParams<any>): Promise<HttpResponse<any>> {
+    const httpResponse = await axios.post(params.url, params.body);
+    return {
+      statusCode: httpResponse.status,
+      body: httpResponse.data
+    };
   }
 }
