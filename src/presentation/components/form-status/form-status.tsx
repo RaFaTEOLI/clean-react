32dab471902477ddea3d17cbeaf124da171131b3
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-04-07 11:32:24 -0300

    feat: ensure Login will render without error wrap

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Spinner from '../spinner/spinner';
 import Styles from './form-status-styles.scss';
+import FormContext from '@/presentation/contexts/form/form-context';
 
 const FormStatus: React.FC = () => {
+  const { isLoading, errorMessage } = useContext(FormContext);
   return (
-    <div className={Styles.errorWrap}>
-      <Spinner className={Styles.spinner} />
-      <span data-testid="main-error" className={Styles.error}>
-        Error
-      </span>
+    <div data-testid="error-wrap" className={Styles.errorWrap}>
+      {isLoading && <Spinner className={Styles.spinner} />}
+      {errorMessage && (
+        <span data-testid="main-error" className={Styles.error}>
+          {errorMessage}
+        </span>
+      )}
     </div>
   );
 };
