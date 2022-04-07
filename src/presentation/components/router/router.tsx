316dc347e0bc655c38913c29f9f2a1f66452319f
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-04-07 11:10:27 -0300

    feat: add app router

---
+++ src/presentation/components/router/router.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Login } from '@/presentation/pages';
+import '@/presentation/styles/global.scss';
+
+const Router: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}>
+          <Route path="login" element={<Login />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
