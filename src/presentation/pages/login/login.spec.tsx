752ecac3afd6613be8f6a24aeafe319aab620f65
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-04-12 20:45:50 -0300

    test: ensure Login shows spinner on submit

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -90,4 +90,20 @@ describe('Login Component', () => {
     const submitButton = sut.getByTestId('submit') as HTMLButtonElement;
     expect(submitButton.disabled).toBeFalsy();
   });
+
+  test('should show spinnder on submit', () => {
+    const { sut } = makeSut();
+
+    const emailInput = sut.getByTestId('email');
+    fireEvent.input(emailInput, { target: { value: faker.internet.email() } });
+
+    const passwordInput = sut.getByTestId('password');
+    fireEvent.input(passwordInput, { target: { value: faker.internet.password() } });
+
+    const submitButton = sut.getByTestId('submit');
+    fireEvent.click(submitButton);
+
+    const spinner = sut.getByTestId('spinner');
+    expect(spinner).toBeTruthy();
+  });
 });
