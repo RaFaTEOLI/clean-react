b5323b60cb8a432cede32c8d5cbb9f804c7cd76a
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-05-10 11:19:26 -0300

    test: ensure ValidationBuilder has EmailValidation

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,9 +1,14 @@
 import { ValidationBuilder as sut } from './validation-builder';
-import { RequiredFieldValidation } from '@/validation/validators';
+import { RequiredFieldValidation, EmailValidation } from '@/validation/validators';
 
 describe('ValidationBuilder', () => {
   test('should return RequiredFieldValidation', () => {
     const validations = sut.field('field').required().build();
     expect(validations).toEqual([new RequiredFieldValidation('field')]);
+  });
+
+  test('should return EmailValidation', () => {
+    const validations = sut.field('field').email().build();
+    expect(validations).toEqual([new EmailValidation('field')]);
   });
 });
