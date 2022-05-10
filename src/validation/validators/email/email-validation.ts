916c42697cfca4901567d9bdceb4d46d676331ce
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-05-10 10:16:07 -0300

    fix: ensure EmailValidation allows empty value

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -5,11 +5,12 @@ export class EmailValidation implements FieldValidation {
   constructor(readonly field: string) {}
 
   validate(value: string): Error {
-    return String(value)
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      )
+    return !value ||
+      String(value)
+        .toLowerCase()
+        .match(
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        )
       ? null
       : new InvalidFieldError();
   }
