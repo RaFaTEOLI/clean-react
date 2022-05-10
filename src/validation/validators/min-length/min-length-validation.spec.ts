544b03d388eb28ff3d040a018ddfaeb4ed71a0a0
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-05-10 10:06:15 -0300

    test: ensure MinLengthValidation returns false if value is valid

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -8,4 +8,10 @@ describe('MinLengthValidation', () => {
     const error = sut.validate('123');
     expect(error).toEqual(new InvalidFieldError());
   });
+
+  test('should return false if value is valid', () => {
+    const sut = new MinLengthValidation(faker.database.column(), 5);
+    const error = sut.validate('12345');
+    expect(error).toBeFalsy();
+  });
 });
