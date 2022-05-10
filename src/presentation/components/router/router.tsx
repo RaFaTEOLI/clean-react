67f6c9e4f58a68c5aaf3c76b2c4a665ac37ad1ca
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-05-10 20:15:29 -0300

    feat: add Login composition

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Login } from '@/presentation/pages';
+import { LoginFactory } from '@/main/factories/pages/login/login-factory';
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />}>
-          <Route path="login" element={<Login />} />
+        <Route path="/" element={<LoginFactory />}>
+          <Route path="login" element={<LoginFactory />} />
         </Route>
       </Routes>
     </BrowserRouter>
