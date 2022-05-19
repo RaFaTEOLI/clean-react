0ad8e8e1ef05014da9433adb6aec2eebc3d7b212
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-05-19 18:54:37 -0300

    test: ensure LocalSaveAccessToken will throw if SetStorage throws

--- src/data/usecases/save-access-token/local-save-access-token.spec.ts
+++ src/data/usecases/save-access-token/local-save-access-token.spec.ts
@@ -25,4 +25,12 @@ describe('LocalSaveAccessToken', () => {
     expect(setStorageMock.key).toBe('accessToken');
     expect(setStorageMock.value).toBe(accessToken);
   });
+
+  test('should throw if SetStorage throws', async () => {
+    const { sut, setStorageMock } = makeSut();
+    jest.spyOn(setStorageMock, 'set').mockRejectedValueOnce(new Error());
+    const accessToken = faker.datatype.uuid();
+    const promise = sut.save(accessToken);
+    await expect(promise).rejects.toThrow(new Error());
+  });
 });
