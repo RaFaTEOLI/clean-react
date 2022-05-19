b0c0071cc486ece49c6031e5783a79f46cb088bc
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-05-19 18:51:10 -0300

    refactor: rename SetStorageSpy to SetStorageMock

--- src/data/test/mock-storage.ts
+++ src/data/test/mock-storage.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { SetStorage } from '@/data/protocols/cache/set-storage';
 
-export class SetStorageSpy implements SetStorage {
+export class SetStorageMock implements SetStorage {
   key: string;
   value: any;
   // eslint-disable-next-line @typescript-eslint/require-await
