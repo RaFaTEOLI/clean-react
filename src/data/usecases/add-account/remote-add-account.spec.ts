395736e2503a29ef411969cb1ea7d4ab78f90be1
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-05-23 18:31:23 -0300

    test: ensure RemoteAddAccount throws UnexpectedError if HttpPostClient returns 400

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -1,6 +1,6 @@
 import { HttpStatusCode } from '@/data/protocols/http';
 import { HttpPostClientSpy } from '@/data/test';
-import { EmailAlreadyBeingUsedError } from '@/domain/errors';
+import { EmailAlreadyBeingUsedError, UnexpectedError } from '@/domain/errors';
 import { AccountModel } from '@/domain/models';
 import { mockAddAccount } from '@/domain/test';
 import { AddAccountParams } from '@/domain/usecases';
@@ -43,5 +43,14 @@ describe('RemoteAddAccount', () => {
     };
     const promise = sut.add(mockAddAccount());
     await expect(promise).rejects.toThrow(new EmailAlreadyBeingUsedError());
+  });
+
+  test('Should throw UnexpectedError if HttpPostClient returns 400', async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.badRequest
+    };
+    const promise = sut.add(mockAddAccount());
+    await expect(promise).rejects.toThrow(new UnexpectedError());
   });
 });
