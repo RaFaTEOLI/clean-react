cce81fd097e60c4324bada347c355a96016ba327
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-05-24 09:32:55 -0300

    feat: add signup view

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { LoginFactory } from '@/main/factories/pages/login/login-factory';
+import { SignUp } from '@/presentation/pages';
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginFactory />}>
-          <Route path="login" element={<LoginFactory />} />
-        </Route>
+        <Route path="/" element={<LoginFactory />} />
+        <Route path="/login" element={<LoginFactory />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
