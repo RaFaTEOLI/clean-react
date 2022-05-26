16389352e4067488cdfce71a597c10ba37edb398
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-05-26 18:38:40 -0300

    chore: setup cypress

--- src/main/test/cypress/plugins/index.js
+++ src/main/test/cypress/plugins/index.js
@@ -1,22 +1,6 @@
-/// <reference types="cypress" />
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
-
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const cypressTypescriptPreProcessor = require('./cy-ts-preprocessor');
-/**
- * @type {Cypress.PluginConfig}
- */
-// eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
-  on('file:preprocessor', cypressTypescriptPreProcessor);
+const cypressTypescriptPreprocessor = require('./cy-ts-preprocessor');
+
+module.exports = on => {
+  on('file:preprocessor', cypressTypescriptPreprocessor);
 };
