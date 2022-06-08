4850779e93fd661d82114158b682c46b18f69c9e
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-06-08 18:44:49 -0300

    feat: ensured RemoteLoadSurveyList will throw UnexpectedError on 403

--- src/data/protocols/http/http-get-client.ts
+++ src/data/protocols/http/http-get-client.ts
@@ -1,7 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { HttpResponse } from '.';
+
 export type HttpGetParams = {
   url: string;
 };
 
-export interface HttpGetClient {
-  get(params: HttpGetParams): Promise<void>;
+export interface HttpGetClient<R = any> {
+  get(params: HttpGetParams): Promise<HttpResponse<R>>;
 }
