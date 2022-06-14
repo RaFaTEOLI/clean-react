c8d7a50954f36d9c6eb863f17ed6253390e65eb9
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-06-14 09:49:32 -0300

    refactor: moved Router to Main Layer

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router } from '@/presentation/components';
+import Router from './routes/router';
 import '@/presentation/styles/global.scss';
 import { createRoot } from 'react-dom/client';
 const container = document.getElementById('main');
