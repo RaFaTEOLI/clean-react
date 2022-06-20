39e493f27c6336ffe3faf72f1184e38a5bc823bf
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-06-20 20:53:14 -0300

    refactor: moved SUT creation to a function method

--- src/presentation/components/private-route/private-route.spec.tsx
+++ src/presentation/components/private-route/private-route.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { createMemoryHistory } from 'history';
+import { createMemoryHistory, MemoryHistory } from 'history';
 import PrivateRoute from './private-route';
 import { Router } from 'react-router-dom';
 
@@ -8,16 +8,25 @@ const Page: React.FC = () => {
   return <h1>Mock Page</h1>;
 };
 
+type SutTypes = {
+  history: MemoryHistory;
+};
+
+const makeSut = (): SutTypes => {
+  const history = createMemoryHistory({ initialEntries: ['/'] });
+  render(
+    <Router navigator={history} location={history.location}>
+      <PrivateRoute>
+        <Page />
+      </PrivateRoute>
+    </Router>
+  );
+  return { history };
+};
+
 describe('PrivateRoute', () => {
   test('should redirect to /login if token is empty', () => {
-    const history = createMemoryHistory({ initialEntries: ['/'] });
-    render(
-      <Router navigator={history} location={history.location}>
-        <PrivateRoute>
-          <Page />
-        </PrivateRoute>
-      </Router>
-    );
+    const { history } = makeSut();
     expect(history.location.pathname).toBe('/login');
   });
 });
