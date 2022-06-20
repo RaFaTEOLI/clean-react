d0788ccc15c0ddec8f56ac1a86f1823049d16a37
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-06-20 18:53:49 -0300

    feat: ensured LocalStorageAdapter will call localStorage.getItem with correct value

--- src/infra/cache/local-storage-adapter.ts
+++ src/infra/cache/local-storage-adapter.ts
@@ -1,8 +1,12 @@
+/* eslint-disable lines-between-class-members */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { SetStorage } from '@/data/protocols/cache/set-storage';
 
 export class LocalStorageAdapter implements SetStorage {
   set(key: string, value: object): void {
     localStorage.setItem(key, JSON.stringify(value));
+  }
+  get(key: string): any {
+    return localStorage.getItem(key);
   }
 }
