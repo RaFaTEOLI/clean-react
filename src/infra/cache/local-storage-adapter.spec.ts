778ca8bb1a929bbafcfa33db1c4f7a5c4bd4b6bc
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-06-20 18:54:19 -0300

    test: ensured LocalStorageAdapter will call localStorage.getItem with correct value

--- src/infra/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -9,11 +9,21 @@ describe('LocalStorageAdapter', () => {
     localStorage.clear();
   });
 
-  test('should call localStorage with correct values', () => {
+  test('should call localStorage.setItem with correct values', () => {
     const sut = makeSut();
     const key = faker.database.column();
     const value = JSON.parse(faker.datatype.json());
     sut.set(key, value);
     expect(localStorage.setItem).toHaveBeenCalledWith(key, JSON.stringify(value));
+  });
+
+  test('should call localStorage.getItem with correct values', () => {
+    const sut = makeSut();
+    const key = faker.database.column();
+    const value = faker.datatype.json();
+    const getItemSpy = jest.spyOn(localStorage, 'getItem').mockReturnValueOnce(value);
+    const obj = sut.get(key);
+    expect(obj).toEqual(value);
+    expect(getItemSpy).toHaveBeenCalledWith(key);
   });
 });
