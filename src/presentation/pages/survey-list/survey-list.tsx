06f24435af1498d4f3afe3e1fa8640626a11cac7
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-06-21 19:21:22 -0300

    feat: ensured SurveyList page will call LoadSurveyList

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,9 +1,19 @@
+import { LoadSurveyList } from '@/domain/usecases';
 import { Footer, Header } from '@/presentation/components';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SurveyItem, SurveyItemEmpty } from './components';
 import Styles from './survey-list-styles.scss';
 
-const SurveyList: React.FC = () => {
+type Props = {
+  loadSurveyList: LoadSurveyList;
+};
+
+const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
+  useEffect(() => {
+    (async () => {
+      await loadSurveyList.all();
+    })();
+  }, []);
   return (
     <div className={Styles.surveyListWrap}>
       <Header />
