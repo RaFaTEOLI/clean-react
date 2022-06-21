774661feabaefa69a3aaab17b8434c732ad9cf8a
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-06-21 19:10:13 -0300

    feat: ensured SurveyList renders 4 empty item on start

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,5 +1,6 @@
-import { Header, Footer, Icon, IconName } from '@/presentation/components';
+import { Footer, Header } from '@/presentation/components';
 import React from 'react';
+import { SurveyItem, SurveyItemEmpty } from './components';
 import Styles from './survey-list-styles.scss';
 
 const SurveyList: React.FC = () => {
@@ -8,22 +9,9 @@ const SurveyList: React.FC = () => {
       <Header />
       <div className={Styles.contentWrap}>
         <h2>Surveys</h2>
-        <ul>
-          <li>
-            <div className={Styles.surveyContent}>
-              <Icon className={Styles.iconWrap} iconName={IconName.thumbDown} />
-              <time>
-                <span className={Styles.day}>22</span>
-                <span className={Styles.month}>03</span>
-                <span className={Styles.year}>2020</span>
-              </time>
-              <p>What is your favorite web framework?</p>
-            </div>
-            <footer>Check Results</footer>
-          </li>
-          <li></li>
-          <li></li>
-          <li></li>
+        <ul data-testid="survey-list">
+          <SurveyItem />
+          <SurveyItemEmpty />
         </ul>
       </div>
       <Footer />
