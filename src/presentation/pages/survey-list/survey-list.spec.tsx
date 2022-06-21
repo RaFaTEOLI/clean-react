f7d213e7f22a57c5d5a95b34e96973c4ddf1dfd6
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-06-21 19:21:49 -0300

    test: ensured SurveyList page will call LoadSurveyList

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -3,15 +3,32 @@ import { render, screen } from '@testing-library/react';
 import { SurveyList } from '@/presentation/pages';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
+import { LoadSurveyList } from '@/domain/usecases';
+import { SurveyModel } from '@/domain/models';
+
+class LoadSurveyListSpy implements LoadSurveyList {
+  callsCount = 0;
+
+  async all(): Promise<SurveyModel[]> {
+    this.callsCount++;
+    return Promise.resolve([]);
+  }
+}
 
 const history = createMemoryHistory({ initialEntries: ['/'] });
 
-const makeSut = (): void => {
+type SutTypes = {
+  loadSurveyListSpy: LoadSurveyListSpy;
+};
+
+const makeSut = (): SutTypes => {
+  const loadSurveyListSpy = new LoadSurveyListSpy();
   render(
     <Router navigator={history} location={history.location}>
-      <SurveyList />
+      <SurveyList loadSurveyList={loadSurveyListSpy} />
     </Router>
   );
+  return { loadSurveyListSpy };
 };
 
 describe('SurveyList Component', () => {
@@ -20,5 +37,10 @@ describe('SurveyList Component', () => {
     const surveyList = screen.getByTestId('survey-list');
 
     expect(surveyList.querySelectorAll('li:empty').length).toBe(4);
+  });
+
+  test('should call LoadSurveyList', () => {
+    const { loadSurveyListSpy } = makeSut();
+    expect(loadSurveyListSpy.callsCount).toBe(1);
   });
 });
