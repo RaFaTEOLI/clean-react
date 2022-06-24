82989414187ca1a60551cd0ca65791ffce0bedf3
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-06-24 12:51:26 -0300

    feat: ensured setCurrentAccount will be called if logout is clicked

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -1,15 +1,28 @@
-import React, { memo } from 'react';
+import React, { memo, useContext } from 'react';
 import Styles from './header-styles.scss';
 import { Logo } from '@/presentation/components';
+import { ApiContext } from '@/presentation/contexts';
+import { useNavigate } from 'react-router';
 
 const Header: React.FC = () => {
+  const navigate = useNavigate();
+  const { setCurrentAccount } = useContext(ApiContext);
+
+  const logout = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void => {
+    event.preventDefault();
+    setCurrentAccount(undefined);
+    navigate('/login');
+  };
+
   return (
     <header className={Styles.headerWrap}>
       <div className={Styles.headerContent}>
         <Logo />
         <div className={Styles.logoutWrap}>
           <span>Rafael</span>
-          <a href="#">Logout</a>
+          <a data-testid="logout" href="#" onClick={logout}>
+            Logout
+          </a>
         </div>
       </div>
     </header>
