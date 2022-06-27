999d013067fa369ebe610ca3d88d7a839c097c36
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-06-27 18:15:58 -0300

    test: ensured header will render username correctly

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -3,7 +3,7 @@ import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { SurveyList } from '@/presentation/pages';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
-import { LoadSurveyListSpy } from '@/domain/test';
+import { LoadSurveyListSpy, mockAccountModel } from '@/domain/test';
 import { UnexpectedError } from '@/domain/errors';
 import { ApiContext } from '@/presentation/contexts';
 
@@ -15,7 +15,7 @@ type SutTypes = {
 
 const makeSut = (legacyRoot = false, loadSurveyListSpy = new LoadSurveyListSpy()): SutTypes => {
   render(
-    <ApiContext.Provider value={{ setCurrentAccount: jest.fn() }}>
+    <ApiContext.Provider value={{ setCurrentAccount: jest.fn(), getCurrentAccount: () => mockAccountModel() }}>
       <Router navigator={history} location={history.location}>
         <SurveyList loadSurveyList={loadSurveyListSpy} />
       </Router>
