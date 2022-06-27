d86e8b8d8cf4b44dd27e4bb721e751a25b20aa6d
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-06-27 18:15:32 -0300

    feat: ensured header will render username correctly

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router';
 
 const Header: React.FC = () => {
   const navigate = useNavigate();
-  const { setCurrentAccount } = useContext(ApiContext);
+  const { setCurrentAccount, getCurrentAccount } = useContext(ApiContext);
 
   const logout = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void => {
     event.preventDefault();
@@ -19,7 +19,7 @@ const Header: React.FC = () => {
       <div className={Styles.headerContent}>
         <Logo />
         <div className={Styles.logoutWrap}>
-          <span>Rafael</span>
+          <span data-testid="username">{getCurrentAccount().name}</span>
           <a data-testid="logout" href="#" onClick={logout}>
             Logout
           </a>
