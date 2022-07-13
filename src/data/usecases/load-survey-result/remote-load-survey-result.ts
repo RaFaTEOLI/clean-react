159d190ed69548a445a360e062acce55f9969b59
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-07-13 18:46:16 -0300

    feat: ensured RemoteLoadSurveyResult will throw AccessDeniedError on 403

--- src/data/usecases/load-survey-result/remote-load-survey-result.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.ts
@@ -1,9 +1,19 @@
-import { HttpGetClient } from '@/data/protocols/http';
+import { HttpGetClient, HttpStatusCode } from '@/data/protocols/http';
+import { AccessDeniedError, UnexpectedError } from '@/domain/errors';
 
 export class RemoteLoadSurveyResult {
   constructor(private readonly url: string, private readonly httpGetClient: HttpGetClient) {}
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   async show(): Promise<void> {
-    await this.httpGetClient.get({ url: this.url });
+    const httpResponse = await this.httpGetClient.get({ url: this.url });
+
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.success:
+        break;
+      case HttpStatusCode.forbidden:
+        throw new AccessDeniedError();
+      default:
+        throw new UnexpectedError();
+    }
   }
 }
