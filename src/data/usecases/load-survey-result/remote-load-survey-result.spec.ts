3b5d603471058a702ab03e15e2b91b282585e14d
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-07-13 18:46:41 -0300

    test: ensured RemoteLoadSurveyResult will throw AccessDeniedError on 403

--- src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
@@ -1,5 +1,7 @@
 import { HttpGetClientSpy } from '@/data/test';
 import { RemoteLoadSurveyResult } from './remote-load-survey-result';
+import { HttpStatusCode } from '@/data/protocols/http';
+import { AccessDeniedError } from '@/domain/errors';
 import { faker } from '@faker-js/faker';
 
 type SutTypes = {
@@ -23,5 +25,14 @@ describe('RemoteLoadSurveyResult', () => {
     const { sut, httpGetClientSpy } = makeSut(url);
     await sut.show();
     expect(httpGetClientSpy.url).toBe(url);
+  });
+
+  test('should throw AccessDeniedError if HttpGetClient returns 403', async () => {
+    const { sut, httpGetClientSpy } = makeSut();
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.forbidden
+    };
+    const promise = sut.show();
+    await expect(promise).rejects.toThrow(new AccessDeniedError());
   });
 });
