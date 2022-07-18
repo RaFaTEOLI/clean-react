961c3237432db9f4b4cf2575504a7da2ba934475
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-07-18 20:30:12 -0300

    test: ensured SurveyResult will have error if request fails

--- src/main/test/cypress/integration/private-route.spec.ts
+++ src/main/test/cypress/integration/private-route.spec.ts
@@ -6,7 +6,7 @@ describe('Private Routes', () => {
     Helper.testUrl('/login');
   });
 
-  it('Should logout if survey-result has no token', () => {
+  it('should logout if survey-result has no token', () => {
     cy.visit('/surveys/any_id');
     Helper.testUrl('/login');
   });
