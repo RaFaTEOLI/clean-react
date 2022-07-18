2952b49fbe5bd899b015f93c1d04861025705d78
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-07-18 18:29:30 -0300

    test: ensured SurveyResult will have error if LoadSurveyResult fails

--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -5,14 +5,13 @@ import { render, screen, waitFor } from '@testing-library/react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import React from 'react';
+import { UnexpectedError } from '@/domain/errors';
 
 type SutTypes = {
   loadSurveyResultSpy: LoadSurveyResultSpy;
 };
 
-const makeSut = (surveyResult = mockSurveyResultModel()): SutTypes => {
-  const loadSurveyResultSpy = new LoadSurveyResultSpy();
-  loadSurveyResultSpy.surveyResult = surveyResult;
+const makeSut = (loadSurveyResultSpy = new LoadSurveyResultSpy()): SutTypes => {
   const history = createMemoryHistory({ initialEntries: ['/surveys'] });
   render(
     <ApiContext.Provider value={{ setCurrentAccount: jest.fn(), getCurrentAccount: () => mockAccountModel() }}>
@@ -42,28 +41,47 @@ describe('SurveyResult Component', () => {
   });
 
   test('should present SurveyResult data on success', async () => {
+    const loadSurveyResultSpy = new LoadSurveyResultSpy();
     const surveyResult = Object.assign(mockSurveyResultModel(), {
       date: new Date('2020-01-10T00:00:00')
     });
-    makeSut(surveyResult);
+    loadSurveyResultSpy.surveyResult = surveyResult;
+    makeSut(loadSurveyResultSpy);
     await waitFor(() => screen.getByTestId('question'));
     expect(screen.getByTestId('day')).toHaveTextContent('10');
     expect(screen.getByTestId('month')).toHaveTextContent('jan');
     expect(screen.getByTestId('year')).toHaveTextContent('2020');
     expect(screen.getByTestId('question')).toHaveTextContent(surveyResult.question);
     expect(screen.getByTestId('answers').childElementCount).toBe(2);
+
     const answersWrap = screen.queryAllByTestId('answer-wrap');
     expect(answersWrap[0]).toHaveClass('active');
     expect(answersWrap[1]).not.toHaveClass('active');
+
     const images = screen.queryAllByTestId('image');
     expect(images[0]).toHaveAttribute('src', surveyResult.answers[0].image);
     expect(images[0]).toHaveAttribute('alt', surveyResult.answers[0].answer);
     expect(images[1]).toBeFalsy();
+
     const answers = screen.queryAllByTestId('answer');
     expect(answers[0]).toHaveTextContent(surveyResult.answers[0].answer);
     expect(answers[1]).toHaveTextContent(surveyResult.answers[1].answer);
+
     const percents = screen.queryAllByTestId('percent');
     expect(percents[0]).toHaveTextContent(`${surveyResult.answers[0].percent}%`);
     expect(percents[1]).toHaveTextContent(`${surveyResult.answers[1].percent}%`);
+  });
+
+  test('should render error on UnexpectedError', async () => {
+    const loadSurveyResultSpy = new LoadSurveyResultSpy();
+    const error = new UnexpectedError();
+    jest.spyOn(loadSurveyResultSpy, 'show').mockRejectedValueOnce(error);
+    makeSut(loadSurveyResultSpy);
+    await waitFor(() => screen.getByTestId('survey-result'));
+    setInterval(() => {
+      expect(screen.queryByTestId('question')).not.toBeInTheDocument();
+      expect(screen.getByTestId('error')).toHaveTextContent(error.message);
+      expect(screen.queryByTestId('loading')).not.toBeInTheDocument();
+    }, 1000);
   });
 });
