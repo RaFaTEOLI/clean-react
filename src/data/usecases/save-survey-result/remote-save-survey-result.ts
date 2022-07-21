2e1f48deee68da9a6e58460c0afdc0f085941802
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-07-21 19:11:46 -0300

    feat: ensured RemoteSaveSurveyResult will return AccessDeniedError on 403

--- src/data/usecases/save-survey-result/remote-save-survey-result.ts
+++ src/data/usecases/save-survey-result/remote-save-survey-result.ts
@@ -1,17 +1,23 @@
-import { HttpClient } from '@/data/protocols/http';
+import { HttpClient, HttpStatusCode } from '@/data/protocols/http';
 import { RemoteSurveyResultModel } from '@/data/models';
 import { SaveSurveyResult } from '@/domain/usecases';
+import { AccessDeniedError } from '@/domain/errors';
 
 export class RemoteSaveSurveyResult implements SaveSurveyResult {
   constructor(private readonly url: string, private readonly httpClient: HttpClient<RemoteSaveSurveyResult.Model>) {}
 
   async save(params: SaveSurveyResult.Params): Promise<SaveSurveyResult.Model> {
-    await this.httpClient.request({
+    const httpResponse = await this.httpClient.request({
       url: this.url,
       method: 'put',
       body: params
     });
-    return Promise.resolve(null);
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.forbidden:
+        throw new AccessDeniedError();
+      default:
+        return Promise.resolve(null);
+    }
   }
 }
 
