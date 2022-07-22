bbf19f0f25ef85ab7a92c4944537939074ac75ff
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-07-22 11:27:30 -0300

    fix: updated class name

--- src/main/factories/pages/survey-result/survey-result-factory.tsx
+++ src/main/factories/pages/survey-result/survey-result-factory.tsx
@@ -3,7 +3,7 @@ import { SurveyResult } from '@/presentation/pages';
 import { useParams } from 'react-router-dom';
 import React from 'react';
 
-export const makeSurveyResult: React.FC = () => {
+export const SurveyResultFactory: React.FC = () => {
   const { id } = useParams();
   return (
     <SurveyResult loadSurveyResult={makeRemoteLoadSurveyResult(id)} saveSurveyResult={makeRemoteSaveSurveyResult(id)} />
